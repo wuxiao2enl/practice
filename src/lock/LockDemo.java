8647d8fca6edf7195e48114184204c977e26e26c
wuxiao2enl <s1026149113@gmail.com>
2020-03-19 15:16:29 +0800

    test commit

---
+++ src/lock/LockDemo.java
@@ -0,0 +1,12 @@
+package lock;
+
+/**
+ *
+ *
+ * @author enlighten 2020-03-19
+ */
+public class LockDemo {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
